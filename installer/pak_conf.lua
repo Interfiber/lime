5fe7074005df374731b80bab3d29607969ed918a
Interfiber <webmaster@interfiber.dev>
2021-10-24 08:47:50 -0400

    :package: Create installer files for later

---
+++ installer/pak_conf.lua
@@ -0,0 +1,16 @@
+return {
+  apperance = {},
+  components = {
+    default = {
+      install_folder = "/opt/lime",
+      name = "Default",
+      payload = "payloads/default",
+      start_selected = true,
+      visible = true
+    }
+  },
+  org_id = "dev.interfiber.lime",
+  pkg_name = "Lime",
+  project_name = "Lime",
+  version = "0.0.1"
+}
